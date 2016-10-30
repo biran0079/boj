22332b35b1532a1e35d0bf2c2c7c86db6d1bc4ed
biran <biran0079@gmail.com>
2016-10-30 14:57:53 -0700

    print vague timestamp

--- src/main/java/com/boj/base/DateUtil.java
+++ src/main/java/com/boj/base/DateUtil.java
@@ -1,6 +1,3 @@
-/*
- * Copyright 2016 Addepar. All rights reserved.
- */
 package com.boj.base;
 
 import java.text.SimpleDateFormat;
@@ -10,13 +7,32 @@ import java.text.SimpleDateFormat;
  */
 public class DateUtil {
 
-  private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+  private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-M-dd HH:mm:ss");
+
+  private static final long ONE_MINUTE_MS = 60L * 1000L;
+  private static final long ONE_HOUR_MS = 60L * ONE_MINUTE_MS;
+  private static final long ONE_DAY_MS = 24L * ONE_HOUR_MS;
+  private static final long ONE_WEEK_MS = 7L * ONE_DAY_MS;
 
   public static int now() {
     return (int) (System.currentTimeMillis() / 1000);
   }
 
   public static String format(int sec) {
-    return FORMAT.format((long) sec * 1000);
+    long now = System.currentTimeMillis();
+    long then = (long) sec * 1000;
+    if (now - then < ONE_MINUTE_MS) {
+      return "just now";
+    }
+    if (now - then < ONE_HOUR_MS) {
+      return "a few minutes ago";
+    }
+    if (now - then < ONE_DAY_MS) {
+      return "a few hours ago";
+    }
+    if (now - then < ONE_WEEK_MS) {
+      return "a few days ago";
+    }
+    return FORMAT.format(then);
   }
 }
