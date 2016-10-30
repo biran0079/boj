01ff7536c0156f314e116757a699fe2a0071691e
biran <biran0079@gmail.com>
2016-10-30 15:34:26 -0700

    use table to show problems

--- src/main/java/com/boj/base/BojExceptionHandler.java
+++ src/main/java/com/boj/base/BojExceptionHandler.java
@@ -3,36 +3,23 @@
  */
 package com.boj.base;
 
-import com.boj.BojServer;
-import com.boj.jooq.tables.records.UserRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import spark.ExceptionHandler;
 import spark.Request;
 import spark.Response;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 
 /**
  * @author Ran Bi (ran.bi@addepar.com)
  */
 public class BojExceptionHandler implements ExceptionHandler {
 
-  private static final Logger logger = LoggerFactory.getLogger(BojServer.class);
-
-  private final Provider<UserRecord> userRecordProvider;
-
   @Inject
-  BojExceptionHandler(Provider<UserRecord> userRecordProvider) {
-    this.userRecordProvider = userRecordProvider;
+  BojExceptionHandler() {
   }
 
   @Override
   public void handle(Exception e, Request request, Response response) {
-    logger.info("User {} does not have permission for request {}.",
-        userRecordProvider.get().getEmail(),
-        request.pathInfo());
     BojException bojException = (BojException) e;
     bojException.getBojErrorType().handle(response);
   }
